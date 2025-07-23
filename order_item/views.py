1f3770ebcb8461939fcb6857abee9b91eb65cd59
NellitG <kamaunelson157@gmail.com>
2025-07-23 16:44:42 +0300

    Add viewsets for all models to provide RESTful API endpoints

--- order_item/views.py
+++ order_item/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from order_item.models import Order_item
+from order_item.serializers import Order_itemSerializer
 
 # Create your views here.
+class Order_itemViewSet(viewsets.ModelViewSet):
+    queryset = Order_item.objects.all()
+    serializer_class = Order_itemSerializer
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Wishlist
+from .serializers import WishlistSerializer
 
 # Create your views here.
+class WishlistViewSet(viewsets.ModelViewSet):
+    queryset = Wishlist.objects.all()
+    serializer_class = WishlistSerializer
