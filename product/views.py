d33099bc15d506e727a5143260167bfbf7cb2f6b
NellitG <kamaunelson157@gmail.com>
2025-07-29 10:59:02 +0300

    add pagination to large datasets

--- product/views.py
+++ product/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Product
 from .serializer import ProductSerializer
+from rest_framework import filters
 
 # Create your views here.
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'description']
