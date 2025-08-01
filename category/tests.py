f49c9f698d198116c09ea5b7ea22ca3dd5a0bd12
NellitG <kamaunelson157@gmail.com>
2025-08-01 14:59:35 +0300

    create test on category

--- category/tests.py
+++ category/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Category
 
 # Create your tests here.
+class CategoryTests(TestCase):
+    def test_category_creation(self):
+        category = Category.objects.create(category_name="Test Category")
+        self.assertEqual(category.category_name, "Test Category")
+        self.assertEqual(str(category), "Test Category")
